6c863b175b395144a6738e84c2d8462fd6c98af4
陳如意 <568iqos@gmail.com>
2026-05-12 14:57:37 +0000

    File changes

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import { ChevronDown } from "lucide-react";
 const DEFAULT_SLIDES = [
   {
-    image: "https://media.base44.com/images/public/69edb64b2f0beef803a1b699/4bf459377_IMG_8758.webp",
+    image: "https://media.base44.com/images/public/69edb64b2f0beef803a1b699/iqos-iluma-japan-limited.webp",
     label: "TEREA Taiwan Official",
     title: "IQOS ILUMA\n韓國限定",
     sub: "每一次點燃，多了一份儀式感\n\n探索日本限定 IQOS ILUMA  與\n各國 TEREA 口味系列",
