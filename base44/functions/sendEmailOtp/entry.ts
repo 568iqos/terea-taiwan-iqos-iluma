953fa1f8048ae0cd9112831035b995b4c3eb6385
陳如意 <568iqos@gmail.com>
2026-05-12 07:55:20 +0000

    File changes

--- base44/functions/sendEmailOtp/entry.ts
+++ base44/functions/sendEmailOtp/entry.ts
@@ -22,7 +22,7 @@ Deno.serve(async (req) => {
     }
 
     // Send email
-    await base44.integrations.Core.SendEmail({
+    await base44.asServiceRole.integrations.Core.SendEmail({
       to: email,
       subject: 'TEREA Taiwan 驗證碼',
       body: `
