116631352386b34d4daeb1b58e29c8b807e3b700
陳如意 <568iqos@gmail.com>
2026-05-12 11:55:32 +0000

    File changes

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -28,7 +28,7 @@ const DEFAULT_SLIDES = [
     overlay: "bg-gradient-to-r from-black/60 via-black/20 to-transparent",
   },
   {
-    image: "https://media.base44.com/images/public/69edb64b2f0beef803a1b699/260e7c9f4_IMG_8125.jpg",
+    image: "https://media.base44.com/images/public/69edb64b2f0beef803a1b699/4d0dfe6db_IMG_8744.jpg",
     label: "ILUMA",
     title: "你的 Q故事\n個性登場",
     sub: "多款配色設計，展現獨一無二的風格品味",
